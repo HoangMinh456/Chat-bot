e02b2350d2adb0c4589899e3ff33490b7f2de50b
Dokhanh15 <dokhanh526@gmail.com>
2024-10-26 20:09:02 +0700

    login and signup

---
+++ FE/src/pages/(website)/login/page.tsx
@@ -0,0 +1,11 @@
+import Login from "./_componnents/Login";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ FE/src/pages/(website)/signup/page.tsx
@@ -0,0 +1,11 @@
+import SignupForm from "./_components/Signup";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
+};
+
+export default SignupPage;
